65b8b94c853b54f0e24ba5535bea0db0605d969d
matrupihai <matrupihai@gmail.com>
2015-06-05 14:32:34 +0300

    first dao test

--- test/com/weblib/dao/AuthorDAOImplTest.java
+++ test/com/weblib/dao/AuthorDAOImplTest.java
@@ -1,17 +1,38 @@
 package com.weblib.dao;
 
-import static org.hamcrest.CoreMatchers.*;
+import static junitparams.JUnitParamsRunner.$;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeFalse;
-import static junitparams.JUnitParamsRunner.*;
 import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.weblib.hbm.model.Author;
 
 @RunWith(JUnitParamsRunner.class)
 public class AuthorDAOImplTest {
 	
+	private AuthorDAOImpl dao = new AuthorDAOImpl();
 	
+	@Before
+	public void initDbConnection() {
+		
+	}
+	
+	@Test
+	@Parameters (method = "paramsForFindAuthorByName")
+	public void find_author_by_name(String name) {
+		Author author = dao.findAuthorByName(name);
+		assertThat(author.getAuthorName(), is(name));
+	}
+	
+	public static Object[] paramsForFindAuthorByName() {
+		return $(
+				$("alber camus")
+				);
+	}
 	
 }
